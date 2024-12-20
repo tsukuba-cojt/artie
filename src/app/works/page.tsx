6207c93859dd8d704a37d78441b35f5b6c8274b1
Shiona1111 <167948961+Shiona1111@users.noreply.github.com>
2024-12-20 16:25:21 +0900

    comi

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,1 +1,22 @@
-ooooooo;
+"use client";
+
+import Header from "@/feature/works/components/Header";
+import SlidingTabs from "@/features/works/components/SlideBar";
+
+export default function Page() {
+  return (
+    <main>
+      <Header />
+
+      <SlidingTabs />
+      <div
+        style={{
+          padding: "20px",
+          textAlign: "center",
+        }}
+      >
+        <p>This is the main content of the page.</p>
+      </div>
+    </main>
+  );
+}
